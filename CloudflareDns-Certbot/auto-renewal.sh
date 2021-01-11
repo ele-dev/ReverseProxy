b9a83d7ec6fb1fb6b36f6248a46015cd4b3374d8
Elias Geiger <elias@ele-dev.de>
2021-01-11 19:32:27 +0100

    updated scripts for easier setup and integration/migration

--- CloudflareDns-Certbot/auto-renewal.sh
+++ CloudflareDns-Certbot/auto-renewal.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# VAR_LOG_DIR="~"
+# VAR_LOG_FILE="ssl-renewal.log"
+
+# Make sure the logfile exists 
+# touch ${VAR_LOG_DIR}/${VAR_LOG_FILE};
+
 echo "Let's Encrypt certificate auto renewal check ..." >> ~/ssl-renewal.log
+# echo "Let's Encrypt certificate auto renewal check ..." >> ${VAR_LOG_DIR}/${VAR_LOG_FILE};
+
 exit
---
+++ CloudflareDns-Certbot/install-cronjob.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# enter the auto-renewal script into the crontab file to be executed periodically
+# ...
+
+exit
