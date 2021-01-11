308d87cc5e5947eb0c67bc388c5d77b5ff34be2c
Elias Geiger <elias@ele-dev.de>
2021-01-11 21:06:21 +0100

    Completed the auto-renewal.sh script

--- CloudflareDns-Certbot/auto-renewal.sh
+++ CloudflareDns-Certbot/auto-renewal.sh
@@ -9,4 +9,7 @@
 echo "Let's Encrypt certificate auto renewal check ..." >> ~/ssl-renewal.log
 # echo "Let's Encrypt certificate auto renewal check ..." >> ${VAR_LOG_DIR}/${VAR_LOG_FILE};
 
+# Run helper container and attempt to renew the existing wildacrd certificate
+docker run --rm -it -v reverseproxy_certs:/etc/letsencrypt eledev/ssl-terminator renew --noninteractive >> ~/ssl-renewal.log
+
 exit
